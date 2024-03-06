70f8e5d0c49c1f82a1e3dc5b4f33e5ffdcbbdf74
wefixte <cassy.pernelle@gmail.com>
2024-03-06 20:51:15 +0100

    Add file and commentary TODO

---
+++ console.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# Add task 6
+# Add task 7
+
+# Add from task 8 :
+# Update your command interpreter (console.py) 
+# to allow show, create, destroy, update and all used with User.
