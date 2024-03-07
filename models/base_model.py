ed2bc468242a89600fbf634f047430be382749ce
git-alexis <alex59.billemont@gmail.com>
2024-03-07 17:18:59 +0100

    delete name and number attribut

--- models/base_model.py
+++ models/base_model.py
@@ -17,8 +17,6 @@ class BaseModel:
             args: set of arguments
             kwargs: set of arguments with keywords
         """
-        self.my_number = 0
-        self.name = ""
         # if kwargs not empty: reconstruct instance from dictionary
         if kwargs:
             for key, value in kwargs.items():
@@ -51,8 +49,6 @@ class BaseModel:
     def to_dict(self):
         """Returns dictionary representation of BaseModel instance"""
         return {
-            "my_number": self.my_number,
-            "name": self.name,
             "__class__": self.__class__.__name__,
             "updated_at": self.updated_at.isoformat(),
             "id": self.id,
